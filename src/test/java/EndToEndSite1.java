ca3351733dd48056e86e9ffb051dd9de7c52e18c
Oleksii <oleksiisuprun@gmail.com>
2018-05-29 15:31:36 +0200

    log4g+report portal is managed

--- src/test/java/EndToEndSite1.java
+++ src/test/java/EndToEndSite1.java
@@ -1,15 +1,19 @@
+import com.epam.reportportal.testng.ReportPortalTestNGListener;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
+@Listeners({ReportPortalTestNGListener.class})
 public class EndToEndSite1 {
     User userData;
     Site0 site1Locators;
 
     @BeforeTest
     public void StartUp() {
+        LoggerHolder.LOGGER.info("sosi pisos1");
         userData = new User();
         site1Locators = new Site0();
     }
@@ -17,7 +21,9 @@ public class EndToEndSite1 {
 
     @Test
     public void EndToEndGuest() throws Exception {
+        LoggerHolder.LOGGER.info("open site");
         open("http://weddingdev.com");
+        LoggerHolder.LOGGER.info("Pass PDP amd add product to Cart");
         $(site1Locators.product1).click();
         $(site1Locators.color).selectOption("white");
         $(site1Locators.size).selectOption("US2");
@@ -25,10 +31,11 @@ public class EndToEndSite1 {
         $(site1Locators.CartLink).click();
 
         $(site1Locators.MinicartViewCartButton).click();
+        LoggerHolder.LOGGER.info("Proceed to checkout");
         $(site1Locators.CartCheckoutButton).click();
         $(site1Locators.GuestLoginContinueButton).click();
 
-        //Shipping
+        LoggerHolder.LOGGER.info("Pass Shipping Step");
         $(site1Locators.FirstName).setValue(userData.FIRSTNAME);
         $(site1Locators.Lastname).setValue(userData.LASTNAME);
         $(site1Locators.EMail).setValue(userData.EMAIL);
@@ -39,14 +46,18 @@ public class EndToEndSite1 {
         $(site1Locators.State).selectOption(userData.STATE);
         $(site1Locators.Password).setValue(userData.PASSWORD);
         $(site1Locators.PasswordConfirm).setValue(userData.PASSWORD);
-
+        LoggerHolder.LOGGER.info("Pass Delivery Step");
         $(site1Locators.ContinueToDeliveryButton).click();
+        LoggerHolder.LOGGER.info("Pass Delivery Method Step");
         $(site1Locators.ContinueToDeliveryMethodButton).click();
+        LoggerHolder.LOGGER.info("Pass Payment Method Step");
         $(site1Locators.ContinueToPaymentMethodButton).click();
+        LoggerHolder.LOGGER.info("Pass Confirm Order Step");
         $(site1Locators.ContinueToConfirmOrderButton).click();
+        LoggerHolder.LOGGER.info("Click on Confirm order button`");
         $(site1Locators.ConfirmOrderButton).click();
 
-        //PayGate
+        LoggerHolder.LOGGER.info("Paygate Shipping");
         $(site1Locators.FirstNameP).setValue(userData.FIRSTNAME);
         $(site1Locators.LastnameP).setValue(userData.LASTNAME);
         $(site1Locators.AddressP).setValue(userData.ADDRESS);
@@ -55,7 +66,7 @@ public class EndToEndSite1 {
         $(site1Locators.TelephoneP).setValue(userData.TELEPHONE);
         $(site1Locators.EMailP).setValue(userData.EMAIL);
 
-
+        LoggerHolder.LOGGER.info("Paygate Billing");
         $(site1Locators.IssuingBank).setValue(userData.BANK);
         $(site1Locators.CardNumber).setValue(userData.CARDNUMBER);
         $(site1Locators.ExpirationMonth).selectOption(userData.EXPMONTH);
---
+++ src/test/java/LoggerHolder.java
@@ -0,0 +1,7 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class LoggerHolder {
+    public static final Logger LOGGER = LogManager.getLogger("binary_data_logger");
+
+}
